fa17760e358cfdb7aee92f27c965cd01e3d69f4c
Cedric <cedricremond@live.nl>
2019-11-17 00:02:25 +0100

    fix open and close sequence

--- box.py
+++ box.py
@@ -6,27 +6,26 @@ from time import sleep
 def open_box():
     print("opening box: unlocking")
     pi = pigpio.pi()
-    pi.set_servo_pulsewidth(17, 2225)
+    pi.set_servo_pulsewidth(17, 2000)
     sleep(0.2)
     pi.set_servo_pulsewidth(17, 0)
     print("opening box: opening lid")
     pi.set_servo_pulsewidth(23, 1725)
     sleep(0.1)
-    tools.write_config("box_status", "open")
 
 
 def close_box():
-    print("closing box: closing lid")
     pi = pigpio.pi()
+    print("closing box: closing lid")
+    pi.set_servo_pulsewidth(23, 0)
     pulse = 1725  # open position of lid
-    while pulse < 1150:  # 1100 close position of lid
+    while pulse > 1100:  # 1100 close position of lid
         pulse -= 25
-        print(pulse)
         pi.set_servo_pulsewidth(23, pulse)
-        sleep(0.05)
+        sleep(0.03)
     pi.set_servo_pulsewidth(23, 0)
     print("closing box: locking")
-    pi.set_servo_pulsewidth(17, 2000)
-    sleep(0.1)
+    sleep(0.2)
+    pi.set_servo_pulsewidth(17, 2250)
+    sleep(1)
     pi.set_servo_pulsewidth(17, 0)
-    tools.write_config("box_status", "closed")
