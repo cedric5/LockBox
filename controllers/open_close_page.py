50861eb5e679a834bcf801f5529acbfc6e2f2a07
cedric5 <cedricremond@live.com>
2020-02-16 23:59:45 +0100

    fix no connection message

--- controllers/open_close_page.py
+++ controllers/open_close_page.py
@@ -24,4 +24,5 @@ def open_close_page_alert():
         return "info", "Your box is not connected to your wifi network. Please setup you wifi connection below."
     if tools.get_config('box_status') == "closed" and tools.get_config('mode') == 'automatic':
         return "danger", "The box is closed, you can not change these settings until the box opens at the set time"
-    return "", ""
+    else:
+        return "", ""
