dc1f671e27bfe6eac3699ad461831cb436468af3
cedric5 <cedricremond@live.com>
2020-02-19 23:07:07 +0100

    fix alert add add comitup client

--- controllers/open_close_page.py
+++ controllers/open_close_page.py
@@ -4,24 +4,22 @@ from flask import request
 
 
 def show():
-    alert = open_close_page_alert()
+
     content_html = open("templates/open-close-settings.html").read()
     template_data = {
         'content': tools.render_page("open-close-settings",
                                      {"open_time": tools.get_config('open_time'),
                                       "close_time": tools.get_config("close_time"),
                                       "box_status": tools.get_config("box_status"),
-                                      "alert": alert[1],
-                                      "alert_type": alert[0],
+                                      "alert": alert()[1],
+                                      "alert_type": alert()[0],
                                       "mode": tools.get_config("mode")
                                       })
     }
     return render_template('main.html', **template_data)
 
 
-def open_close_page_alert():
-    if not tools.has_internet_connection():
-        return "info", "Your box is not connected to your wifi network. Please setup you wifi connection below."
+def alert():
     if tools.get_config('box_status') == "closed" and tools.get_config('mode') == 'automatic':
         return "danger", "The box is closed, you can not change these settings until the box opens at the set time"
     else:
--- controllers/wifi_page.py
+++ controllers/wifi_page.py
@@ -6,7 +6,19 @@ from flask import request
 def show():
     template_data = {
         'content': tools.render_page("wifi-settings",
-                               {'wifi_ssid': tools.get_config('wifi_ssid'), 'wifi_wpa2': tools.get_config('wifi_wpa2')})
+                               {
+                                'wifi_ssid': tools.get_config('wifi_ssid'),
+                                'wifi_wpa2': tools.get_config('wifi_wpa2'),
+                                "alert": alert()[1],
+                                "alert_type": alert()[0],
+                               })
     }
     return render_template('main.html', **template_data)
+
+def alert():
+    if  tools.has_internet_connection():
+        return ["info", "Your box is not connected to your wifi network please setup you wifi connection below."]
+    else:
+        return ["", ""]
+
 
